e079ef11ba57fad114cee6cdd807026a88cb528a
Maksim Knokhinov <maxim122423@gmail.com>
2023-02-08 21:01:25 -0500

    AssetManager

--- Advanced_Graphics_Assignment/AssetManager.cpp
+++ Advanced_Graphics_Assignment/AssetManager.cpp
@@ -1,1 +1,27 @@
 #include "AssetManager.h"
+
+AssetManager* AssetManager::sInstance = nullptr;
+
+AssetManager* AssetManager::Instance()
+{
+	if (sInstance == nullptr) {
+		sInstance = new AssetManager();
+	}
+
+	return sInstance;
+}
+
+void AssetManager::Release()
+{
+	delete sInstance;
+	sInstance = nullptr;
+}
+
+AssetManager::~AssetManager()
+{
+	
+}
+
+AssetManager::AssetManager() {
+
+}
--- Advanced_Graphics_Assignment/AssetManager.h
+++ Advanced_Graphics_Assignment/AssetManager.h
@@ -1,5 +1,20 @@
-#pragma once
+#ifndef _ASSETMANAGER_H
+#define _ASSETMANAGER_H
+
+#include "Shader.h"
+#include "Texture.h"
+
 class AssetManager
 {
+
+public:
+	static AssetManager* Instance();
+	static AssetManager* sInstance;
+	static void Release();
+
+private:
+	AssetManager();
+	~AssetManager();
 };
 
+#endif
