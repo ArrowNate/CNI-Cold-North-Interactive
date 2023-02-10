72267df5aea3f603ac7a601f6c41ac7c7d61b96b
Droidsbane <116039533+Droidsbane@users.noreply.github.com>
2023-02-10 13:26:32 -0500

    Update AudioManager.h

--- Advanced_Graphics_Assignment/AudioManager.h
+++ Advanced_Graphics_Assignment/AudioManager.h
@@ -29,7 +29,7 @@ public:
 	~AudioManager();
 
 	void PlayMusic();
-	void PlayMusic2(const char filepath[50], bool loop = true);
+	void PlayMusic(const char filepath[50], bool loop = true);
 	void PlayMusic3D(const char filepath[50], float x, float y, float z, bool loop = true);
 	void PauseMusic();
 	void PlaySFX();
