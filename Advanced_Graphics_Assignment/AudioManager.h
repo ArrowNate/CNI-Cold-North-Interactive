cea4ec784d5c889267a25de4b1a95e4aea34cd6c
Droidsbane <116039533+Droidsbane@users.noreply.github.com>
2023-02-10 19:42:05 -0500

    g

--- Advanced_Graphics_Assignment/AudioManager.h
+++ Advanced_Graphics_Assignment/AudioManager.h
@@ -30,7 +30,10 @@ public:
 
 	void PlayMusic();
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+>>>>>>> parent of 72267df (Update AudioManager.h)
 	void PlayMusic2(const char filepath[50], bool loop = true);
 	void PlayMusic3D(const char filepath[50], float x, float y, float z, bool loop = true);
 >>>>>>> parent of 72267df (Update AudioManager.h)
